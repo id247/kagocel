4560f644ab0af7ac8282f485337afce7c7d47f05
simplecss <simplecss@yandex.ru>
2016-10-20 16:07:17 +0300

    mosreg added

--- src/js/settings/settings-mosreg.js
+++ src/js/settings/settings-mosreg.js
@@ -1,23 +1,22 @@
 export const OAuthOptions = {
-	provider: 'app',
-	authUrl: 'https://login.staging.dnevnik.ru/oauth2',
-	grantUrl: 'https://api.staging.dnevnik.ru/v1/authorizations',
-	scope: 'Avatar,FullName,Birthday,Age,Roles,Schools,Organizations,EduGroups,Lessons,Marks,EduWorks,Relatives,Files,Contacts,Friends,Groups,Networks,Events,Wall,Messages,EmailAddress,Sex,SocialEntityMembership',	
-	clientId: '5123975fe9eb415390fb7aa316a15e4e',
-	redirectUrl: '//localhost:9000/oauth.html',
+	provider: 'kagocelMosreg',
+	authUrl: 'https://login.school.mosreg.ru/oauth2',
+	grantUrl: 'https://api.school.mosreg.ru/v1/authorizations',
+	scope: 'Roles',	
+	clientId: 'f803ffd2527b4e98816785b9a144f779',
+	redirectUrl: 'https://ad.school.mosreg.ru/promo/oauth2',
 }
 
 export const APIoptions = {	
-	base: 'https://api.staging.dnevnik.ru/v1/',
+	base: 'https://api.school.mosreg.ru/v1/',
 }
 
 export const PromoOptions = {	
-	url: 'http://localhost:9000',
-	server: 'https://staging.dnevnik.ru',
+	url: 'https://ad.school.mosreg.ru/promo/kagocel-2016',
+	server: 'https://school.mosreg.ru',
 	cdn: 'https://ad.csdnevnik.ru/special/staging/kagocel-2016/',
 	doctorName: 'Цельс',
 	bgClick: 'https://gderu.hit.gemius.pl/lshitredir/id=NDVQe81U5z1SAAeq0TS2zn14HwEGQq8T1eq3nO99mVz.07/fastid=euaruvwbdztzrdkqeuitzelbuznw/stparam=xairorftsx/url=http://www.kagocel.ru/promo/?utm_source=dnevnik.ru&utm_medium=&utm_campaign=01.09.2016_31.10.2016_BackgroundBonus-&utm_term=BackgroundBonus&utm_content=1472478538',
-
 }
 
 export const CommentsOptions = {	
@@ -25,5 +24,5 @@ export const CommentsOptions = {
 	adminId: [
 		'1000005031742'
 	],
-	anonAvatar: 'https://static.dnevnik.ru/images/avatars/user/a.m.jpg',
+	anonAvatar: 'https://static.school.mosreg.ru/images/avatars/user/a.m.jpg',
 }
